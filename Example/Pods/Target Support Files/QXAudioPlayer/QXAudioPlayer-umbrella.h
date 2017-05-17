2c454ce2cbf9d60adee861e006a05cc0539e8539
mengqingxiang@jiashuangkuaizi.com <703856207@qq.com>
2017-05-17 18:24:19 +0800

    fix bug

--- Example/Pods/Target Support Files/QXAudioPlayer/QXAudioPlayer-umbrella.h
+++ Example/Pods/Target Support Files/QXAudioPlayer/QXAudioPlayer-umbrella.h
@@ -2,6 +2,11 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "NSURL+QXURL.h"
+#import "QXAudioCache.h"
+#import "QXAudioPlayDownLoader.h"
+#import "QXAudioPlayer.h"
+#import "QXAudioPlayerLoadDelegate.h"
 
 FOUNDATION_EXPORT double QXAudioPlayerVersionNumber;
 FOUNDATION_EXPORT const unsigned char QXAudioPlayerVersionString[];
